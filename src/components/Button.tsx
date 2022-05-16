5c3aaad47b392609aeaae0eba9c023c6fac638b9
Ilya Bayanov <i.p.bayanov@gmail.com>
2022-05-17 01:00:50 +0300

    implement Button component

---
+++ src/components/Button.tsx
@@ -0,0 +1,11 @@
+import { IButtonProps } from "../interfaces-and-types";
+
+export default function Button(props: IButtonProps) {
+  const { width, height, caption, visible } = props;
+
+  return (
+    <button style={{height, width}} hidden={!visible}>
+      {caption}
+    </button>
+  );
+}

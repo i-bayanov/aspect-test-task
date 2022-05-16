707297fca49dfd53b29145f9b5d0e58307a5b614
Ilya Bayanov <i.p.bayanov@gmail.com>
2022-05-17 01:00:13 +0300

    implement Panel component

---
+++ src/components/Panel.tsx
@@ -0,0 +1,16 @@
+import { IPanelProps } from "../interfaces-and-types";
+
+import Content from "../Content";
+
+export default function Panel(props: IPanelProps) {
+  const { width, height, visible, content } = props;
+
+  return (
+    <div
+      style={{ width, height , border: '1px solid black'}}
+      hidden={!visible}
+    >
+      {content && content.map((item, idx) => <Content key={idx} {...item}/>)}
+    </div>
+  );
+}

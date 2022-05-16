dd7f71e167f3aea7d321f19142d7ed2fa908e9e1
Ilya Bayanov <i.p.bayanov@gmail.com>
2022-05-17 00:31:56 +0300

    implement index

---
+++ src/index.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+
+import { Provider } from 'react-redux';
+
+import { store } from './store/store';
+import App from './App';
+
+const rootContainer = document.getElementById('root')!;
+const root = createRoot(rootContainer);
+
+root.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>
+);

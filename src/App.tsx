1057846a65b26aa9cfcfc60dfdcb75edca20254a
Ilia Bayanov <ilia_bayanov@epam.com>
2022-05-17 18:22:56 +0300

    add markup for form

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,25 @@ import { RootState } from './store/store';
 
 import Content from './Content';
 
+import './App.css';
+
 export default function App() {
   const content = useAppSelector((state: RootState) => state);
 
   return (
     <div className="App">
-      {content.map((item, idx) => <Content key={idx} {...item} />)}
+      <form>
+        <label htmlFor='path'>Путь
+          <input type={'text'} id='path' />
+        </label>
+        <label htmlFor='value'>Новое значение
+          <input type={'text'} id='value' />
+        </label>
+        <button className='submit'>Применить</button>
+      </form>
+      <div className='content_wrapper'>
+        {content.map((item, idx) => <Content key={idx} {...item} />)}
+      </div>
     </div>
   );
 }

834423c921ad593de5b38d7c158c6f6a307d59c8
Ilia Bayanov <ilia_bayanov@epam.com>
2022-05-19 20:03:45 +0300

    add function for "edit" action type, add function for deep clone

--- src/store/reduser.ts
+++ src/store/reduser.ts
@@ -10,8 +10,35 @@ export default function contentReducer(
     case 'content/add':
       return store;
     case 'content/edit':
-      return store;
+      const newStore = makeDeepObjectClone(store);
+      action.payload.destination.reduce((acc, curr, idx, arr) => {
+        // @ts-ignore curr has "any" type
+        if (idx === arr.length - 1) acc[curr] = action.payload.newValue;
+        // @ts-ignore curr has "any" type
+        acc = acc[curr];
+
+        return acc;
+      }, newStore);
+
+      return newStore;
     default:
       return store;
   }
+}
+
+function makeDeepObjectClone(obj: object) {
+  const clone = Object.create(Object.getPrototypeOf(obj));
+
+  Reflect.ownKeys(obj).forEach((key) => {
+    const descriptor = Object.getOwnPropertyDescriptor(obj, key)!;
+
+    if (typeof (descriptor.value) === 'object') {
+      // @ts-ignore element has "any" type
+      descriptor.value = makeDeepObjectClone(obj[key]);
+    }
+
+    Object.defineProperty(clone, key, descriptor);
+  });
+
+  return clone;
 }

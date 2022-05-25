ac97e47943e6a07e51b5821fafa137257e560bcf
Ilia Bayanov <ilia_bayanov@epam.com>
2022-05-25 20:02:56 +0300

    change mock due to changes in interface

--- src/store/mockContent.ts
+++ src/store/mockContent.ts
@@ -7,42 +7,42 @@ const MockContent: Array<IPanel | ILabel | IButton> = [
       width: 500,
       height: 200,
       visible: true,
-      content: [
-        {
-          type: 'label',
-          props: {
-            caption: 'test inside panel',
-            visible: true,
-          },
-        },
-        {
-          type: 'button',
-          props: {
-            width: 100,
-            height: 50,
-            visible: true,
-            caption: 'button inside panel',
-          },
-        },
-        {
-          type: 'panel',
-          props: {
-            width: 200,
-            height: 100,
-            visible: false,
-            content: [
-              {
-                type: 'label',
-                props: {
-                  caption: 'one more text',
-                  visible: true,
-                },
-              },
-            ],
-          },
-        },
-      ],
     },
+    content: [
+      {
+        type: 'label',
+        props: {
+          caption: 'test inside panel',
+          visible: true,
+        },
+      },
+      {
+        type: 'button',
+        props: {
+          width: 100,
+          height: 50,
+          visible: true,
+          caption: 'button inside panel',
+        },
+      },
+      {
+        type: 'panel',
+        props: {
+          width: 200,
+          height: 100,
+          visible: false,
+        },
+        content: [
+          {
+            type: 'label',
+            props: {
+              caption: 'one more text',
+              visible: true,
+            },
+          },
+        ],
+      },
+    ],
   },
   {
     type: 'label',

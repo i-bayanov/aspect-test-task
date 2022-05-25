d5bc665166fd3573cd0c8adca97973b2230144a9
Ilia Bayanov <ilia_bayanov@epam.com>
2022-05-25 20:00:18 +0300

    change code due changes in interface IPanelProps, shorten styles

--- src/components/Panel.tsx
+++ src/components/Panel.tsx
@@ -1,13 +1,13 @@
-import { IPanelProps } from "../interfaces-and-types";
+import { IButton, ILabel, IPanel, IPanelProps } from "../interfaces-and-types";
 
 import Content from "../Content";
 
-export default function Panel(props: IPanelProps) {
+export default function Panel(props: IPanelProps & { content: Array<IPanel | ILabel | IButton> }) {
   const { width, height, visible, content } = props;
 
   return (
     <div
-      style={{ height: `${height}px`, width: `${width}px`, border: '1px solid black' }}
+      style={{ height, width, border: '1px solid black' }}
       hidden={!visible}
     >
       {content && content.map((item, idx) => <Content key={idx} {...item} />)}
